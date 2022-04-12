3d23552281479c0f0daa1e354f20908b849043ce
McGrath <pmcgrath7@ipsec-10-2-224-83.vpn.gatech.edu>
2022-04-12 16:06:25 -0400

    Initial fixes

--- cichlid_sr_sequencing/createTestData.py
+++ cichlid_sr_sequencing/createTestData.py
@@ -10,11 +10,11 @@ os.makedirs(fm_obj.localReadsDir + 'Test')
 fq1 = fm_obj.localReadsDir + 'PRJEB15289/ERR3634107_1.fastq.gz'
 fq2 = fm_obj.localReadsDir + 'PRJEB15289/ERR3634107_2.fastq.gz'
 
-fm_obj.downloadData(fq1)
-fm_obj.downloadData(fq2)
+#fm_obj.downloadData(fq1)
+#fm_obj.downloadData(fq2)
 
-subprocess.call(['gunzip', fq1])
-subprocess.call(['gunzip', fq2])
+#subprocess.call(['gunzip', fq1])
+#subprocess.call(['gunzip', fq2])
 
 subprocess.call(['head', '-1600', fq1.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'Test/Test_1.fastq', 'w'))
 subprocess.call(['head', '-1600', fq2.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'Test/Test_2.fastq', 'w'))
@@ -28,7 +28,7 @@ new_data['RunID'] = 'Test'
 new_data['ProjectID'] = 'Test'
 new_data['Files'] = fm_obj.localReadsDir + 'Test/Test_1.fastq,,' + fm_obj.localReadsDir + 'Test/Test_2.fastq'
 
-s_dt = s_dt.append(new_data)
+s_dt = s_dt.append(new_dat, ignore_index = True)
 s_dt.to_csv(fm_obj.localSampleFile, index = False)
 
 pdb.set_trace()
