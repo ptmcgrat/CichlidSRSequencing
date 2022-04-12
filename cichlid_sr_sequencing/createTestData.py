f03777412e8f6f69068327d33ec081b028767318
McGrath <pmcgrath7@ipsec-10-2-224-83.vpn.gatech.edu>
2022-04-12 15:48:02 -0400

    Initial fixes

--- cichlid_sr_sequencing/createTestData.py
+++ cichlid_sr_sequencing/createTestData.py
@@ -9,14 +9,14 @@ s_dt = pd.read_csv(fm_obj.localSampleFile)
 fq1 = fm_obj.localReadsDir + 'PRJEB15289/ERR3634107_1.fastq.gz'
 fq2 = fm_obj.localReadsDir + 'PRJEB15289/ERR3634107_2.fastq.gz'
 
-fm_obj.downloadData(fq1)
-fm_obj.downloadData(fq2)
+#fm_obj.downloadData(fq1)
+#fm_obj.downloadData(fq2)
 
-subprocess.call(['gunzip', fq1])
-subprocess.call(['gunzip', fq2])
+#subprocess.call(['gunzip', fq1])
+#subprocess.call(['gunzip', fq2])
 
-subprocess.call(['head', '1600', fq1.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'PRJEB15289/Test_1.fastq', 'w'))
-subprocess.call(['head', '1600', fq2.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'PRJEB15289/Test_2.fastq', 'w'))
+subprocess.call(['head', '-1600', fq1.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'PRJEB15289/Test_1.fastq', 'w'))
+subprocess.call(['head', '-1600', fq2.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'PRJEB15289/Test_2.fastq', 'w'))
 
 pdb.set_trace()
 
