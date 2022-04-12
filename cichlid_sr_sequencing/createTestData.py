e434dbe52de02f90b4cc8f4e6587b6d2fd1f3365
McGrath <pmcgrath7@ipsec-10-2-224-83.vpn.gatech.edu>
2022-04-12 15:53:52 -0400

    Initial fixes

--- cichlid_sr_sequencing/createTestData.py
+++ cichlid_sr_sequencing/createTestData.py
@@ -10,11 +10,11 @@ os.makedirs(fm_obj.localReadsDir + 'Test')
 fq1 = fm_obj.localReadsDir + 'PRJEB15289/ERR3634107_1.fastq.gz'
 fq2 = fm_obj.localReadsDir + 'PRJEB15289/ERR3634107_2.fastq.gz'
 
-#fm_obj.downloadData(fq1)
-#fm_obj.downloadData(fq2)
+fm_obj.downloadData(fq1)
+fm_obj.downloadData(fq2)
 
-#subprocess.call(['gunzip', fq1])
-#subprocess.call(['gunzip', fq2])
+subprocess.call(['gunzip', fq1])
+subprocess.call(['gunzip', fq2])
 
 subprocess.call(['head', '-1600', fq1.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'Test/Test_1.fastq', 'w'))
 subprocess.call(['head', '-1600', fq2.replace('.gz','')], stdout = open(fm_obj.localReadsDir + 'Test/Test_2.fastq', 'w'))
