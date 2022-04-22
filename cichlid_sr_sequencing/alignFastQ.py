e29760cf5098dd63892359c962a7de58ef8068d2
McGrath <pmcgrath7@cos-2A09148.local>
2022-04-22 09:13:09 -0400

    Initial fixes

--- cichlid_sr_sequencing/alignFastQ.py
+++ cichlid_sr_sequencing/alignFastQ.py
@@ -54,7 +54,7 @@ existing_bams = list(a_dt.SampleID)
 
 
 for sample in good_samples:
-	if sample in ['SAMEA1904331']:
+	if sample in ['SAMEA1904331', 'SAMEA1904330']:
 		print('Skipping ' + sample + '. Problematic.')
 		continue
 	if sample in existing_bams:
