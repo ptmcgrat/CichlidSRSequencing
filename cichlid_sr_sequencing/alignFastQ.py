98a87620bc315b853f8ae8a4a2190d5069f5331d
McGrath <pmcgrath7@ipsec-10-2-225-79.vpn.gatech.edu>
2022-05-09 14:54:09 -0400

    Initial fixes

--- cichlid_sr_sequencing/alignFastQ.py
+++ cichlid_sr_sequencing/alignFastQ.py
@@ -54,7 +54,7 @@ existing_bams = list(a_dt.SampleID)
 
 
 for sample in good_samples:
-	if sample in ['SAMEA1904331', 'SAMEA1904330', 'SAMEA1904323', 'SAMEA4032094', 'SAMEA1904322', 'SAMEA4032090', 'SAMEA1904329', 'SAMEA1904328', 'SAMEA4032091']:
+	if sample in ['SAMEA1904331', 'SAMEA1904330', 'SAMEA1904323', 'SAMEA4032094', 'SAMEA1904322', 'SAMEA4032090', 'SAMEA1904329', 'SAMEA1904328', 'SAMEA4032091', 'SAMEA1920092']:
 		print('Skipping ' + sample + '. Problematic.')
 		continue
 	if sample in existing_bams:
