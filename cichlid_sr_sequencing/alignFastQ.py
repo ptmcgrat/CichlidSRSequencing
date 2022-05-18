1de9d3564cc5bc87937cd1efdce94d36abff75b5
McGrath <pmcgrath7@ipsec-10-2-96-253.vpn.gatech.edu>
2022-05-18 09:13:36 -0400

    Initial commits

--- cichlid_sr_sequencing/alignFastQ.py
+++ cichlid_sr_sequencing/alignFastQ.py
@@ -31,7 +31,7 @@ s_dt = pd.read_csv(fm_obj.localSampleFile)
 if args.ProjectID is not None:
 	if args.ProjectID not in s_dt.ProjectID:
 		pdb.set_trace()
-		raise argparse.ArgumentTypeError('ProjectID does not exist. Options are: ' + ','.join(set(s_dt.ProjectID)))
+		raise argparse.ArgumentTypeError('ProjectID ' + args.ProjectID + ' does not exist. Options are: ' + ','.join(set(s_dt.ProjectID)))
 	s_dt = s_dt[s_dt.ProjectID == args.ProjectID]
 	good_samples = set(s_dt.SampleID)
 
