92f9c915065e2df56b68bd0cfd569f338749d320
ptmcgrat <pmcgrath7@gatech.edu>
2022-08-18 12:03:28 -0400

    Local Download

--- cichlid_sr_sequencing/unit_scripts/grabENA.py
+++ cichlid_sr_sequencing/unit_scripts/grabENA.py
@@ -3,41 +3,52 @@ from helper_modules.file_manager import FileManager as FM
 
 parser = argparse.ArgumentParser(usage = 'This script grabs the ENA data for a run and uploads it to dropbox')
 parser.add_argument('RunID', type = str, help = 'File containing information on each run')
-parser.add_argument('ENA_fq1', type = str, help = 'File containing information on each run')
-parser.add_argument('ENA_fq2', type = str, help = 'File containing information on each run')
+parser.add_argument('fq1', type = str, help = 'File containing information on each run')
+parser.add_argument('fq2', type = str, help = 'File containing information on each run')
 parser.add_argument('OutputBam', type = str, help = 'Location of output bam files')
 parser.add_argument('Temp_directory', type = str, help = 'Directory to temporarily hold datafiles')
 parser.add_argument('SampleName', type = str, help = 'Info for creating read group tag for bam file')
 parser.add_argument('LibraryName', type = str, help = 'Info for creating read group tag for bam file')
 parser.add_argument('Platform', type = str, help = 'Info for creating read group tag for bam file')
 parser.add_argument('-t', '--TestData', action = 'store_true', help = 'Use this flag if you want to create a small test file (1000 reads) instead of the entire read set')
+parser.add_argument('-l', '--Local', action = 'store_true', help = 'Use this flag if the read data is on Dropbox')
 
 args = parser.parse_args()
 
 fm_obj = FM()
 
 target_directory = args.Temp_directory
-local_fq1 = args.Temp_directory + args.RunID + '_1.fastq.gz'
-local_fq2 = args.Temp_directory + args.RunID + '_2.fastq.gz'
+
+if args.Local:
+	local_fq1 = args.fq1
+	local_fq2 = args.fq2
+else:
+	local_fq1 = args.Temp_directory + args.RunID + '_1.fastq.gz'
+	local_fq2 = args.Temp_directory + args.RunID + '_2.fastq.gz'
 temp_bam_file = args.Temp_directory + args.RunID + '_temp.bam'
 
-#target_directory = args.Local_fq1.replace(args.Local_fq1.split('/')[-1],'')
-print('  Fastq files acsping for ' + args.RunID + ', Time:' + str(datetime.datetime.now()))
-for i in range(3):
-	output = subprocess.run(['ascp', '-QT', '-l', '1000m', '-P', '33001', '-i', os.getenv('HOME') + '/anaconda3/envs/CichlidSRSequencing/etc/asperaweb_id_dsa.openssh', args.ENA_fq1.replace('ftp.sra.ebi.ac.uk/','era-fasp@fasp.sra.ebi.ac.uk:'),target_directory], capture_output = True)
-	if output.returncode == 0:
-		break
-	elif i == 2:
-		sys.exit()
-	print('Redownloading ' + args.RunID + ' try ' + str(i + 1))
+if args.Local:
+	fm_obj.downloadData(args.fq1)
+	fm_obj.downloadData(args.fq2)
+else:
 
-for i in range(3):
-	output = subprocess.run(['ascp', '-QT', '-l', '1000m', '-P', '33001', '-i', os.getenv('HOME') + '/anaconda3/envs/CichlidSRSequencing/etc/asperaweb_id_dsa.openssh', args.ENA_fq2.replace('ftp.sra.ebi.ac.uk/','era-fasp@fasp.sra.ebi.ac.uk:'),target_directory], capture_output = True)
-	if output.returncode == 0:
-		break
-	elif i == 2:
-		sys.exit()
-	print('Redownloading ' + args.RunID + ' try ' + str(i + 1))
+	#target_directory = args.Local_fq1.replace(args.Local_fq1.split('/')[-1],'')
+	print('  Fastq files acsping for ' + args.RunID + ', Time:' + str(datetime.datetime.now()))
+	for i in range(3):
+		output = subprocess.run(['ascp', '-QT', '-l', '1000m', '-P', '33001', '-i', os.getenv('HOME') + '/anaconda3/envs/CichlidSRSequencing/etc/asperaweb_id_dsa.openssh', args.ENA_fq1.replace('ftp.sra.ebi.ac.uk/','era-fasp@fasp.sra.ebi.ac.uk:'),target_directory], capture_output = True)
+		if output.returncode == 0:
+			break
+		elif i == 2:
+			sys.exit()
+		print('Redownloading ' + args.RunID + ' try ' + str(i + 1))
+
+	for i in range(3):
+		output = subprocess.run(['ascp', '-QT', '-l', '1000m', '-P', '33001', '-i', os.getenv('HOME') + '/anaconda3/envs/CichlidSRSequencing/etc/asperaweb_id_dsa.openssh', args.ENA_fq2.replace('ftp.sra.ebi.ac.uk/','era-fasp@fasp.sra.ebi.ac.uk:'),target_directory], capture_output = True)
+		if output.returncode == 0:
+			break
+		elif i == 2:
+			sys.exit()
+		print('Redownloading ' + args.RunID + ' try ' + str(i + 1))
 
 # Convert fastq files to unmapped bam
 print('  Converting fastq files to uBam file')
