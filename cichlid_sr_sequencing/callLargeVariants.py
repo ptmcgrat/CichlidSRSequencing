d9d27a87e65b09e65f16cba7bf84c918b4d009f7
McGrath <pmcgrath7@ipsec-10-2-98-226.vpn.gatech.edu>
2022-08-26 09:48:00 -0400

    Fix Fastq files

--- cichlid_sr_sequencing/callLargeVariants.py
+++ cichlid_sr_sequencing/callLargeVariants.py
@@ -24,6 +24,9 @@ sampleIDs = set(a_dt[a_dt.Ecogroup != 'Riverine'].SampleID)
 
 bamfiles = []
 count = 0
+
+pdb.set_trace()
+
 for sampleID in sampleIDs:
 	print(sampleID)
 	fm_obj.createBamFiles(sampleID)
