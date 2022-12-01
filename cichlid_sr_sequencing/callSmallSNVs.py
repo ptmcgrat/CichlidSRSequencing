0f190fcda19c6fb57a3b050d986aabcbb95caac6
nkumar317 <nkumar317@gatech.edu>
2022-12-01 13:40:18 -0500

    test upload

--- cichlid_sr_sequencing/callSmallSNVs.py
+++ cichlid_sr_sequencing/callSmallSNVs.py
@@ -1,5 +1,5 @@
 # test line
-import argparse, pdb, subprocess, pysam
+import argparse, pdb, subprocess as sp, pysam
 from helper_modules.file_manager import FileManager as FM
 from helper_modules.Timer import Timer
 
@@ -29,30 +29,40 @@ gvcffiles = []
 # Create timer object
 timer = Timer()
 
-#fm_obj.downloadData(fm_obj.localGenomeDir)
-#fasta_obj = pysam.FastaFile(fm_obj.localGenomeFile)
+# # Download the reference genome files and build the proper directry structure for them locally. 
+# fm_obj.downloadData(fm_obj.localGenomeDir)
+fasta_obj = pysam.FastaFile(fm_obj.localGenomeFile)
+# below line defines the LG names for the first 22 LGs in the genome. 
+contigs = fasta_obj.references[0:22]
+# # Download the GVCF and GVCF.idx files for each sample in the AlignmentDatabase
+# for sampleID in sampleIDs:
+# 	if sampleID in []:
+# 		continue
+# 	print(sampleID)
+# 	fm_obj.createSampleFiles(sampleID)
+# 	fm_obj.downloadData(fm_obj.localGVCFFile)
+# 	fm_obj.downloadData(fm_obj.localGVCFFile + '.tbi')
+# 	gvcffiles.append(fm_obj.localGVCFFile)
+# 	# pdb.set_trace() # used for troubleshooting
 
-for sampleID in sampleIDs:
-	if sampleID in []:
-		continue
-	print(sampleID)
-	fm_obj.createSampleFiles(sampleID)
-	fm_obj.downloadData(fm_obj.localGVCFFile)
-	fm_obj.downloadData(fm_obj.localGVCFFile + '.tbi')
-	gvcffiles.append(fm_obj.localGVCFFile)
-	pdb.set_trace()
+for contig in contigs:
+	p1 = sp.Popen([f('gatk', 'GenomicsDBImport', '--genomicsdb-workspace-path', 'my_database_{contigs}', '--intervals', {contigs}, '--sample-name-map', 'test_sample_map.txt', '--reader-threads', '4')])
 
+
+
+
+"""
 processes = []
 for contig in fasta_obj.references:
 	#timer.start('Calling SNVs for ' + str(len(bamfiles)) + ' bamfiles.')
 
-	# p1 = subprocess.Popen(['bcftools', 'mpileup', '-r', contig, '-C', '50', '-pm2', '-F', '0.2', '-f', fm_obj.localGenomeFile] + bamfiles, stdout = subprocess.PIPE)
-	# processes.append(subprocess.Popen(['bcftools', 'call', '-vmO', 'v', '-f', 'GQ', '-o', contig + '.vcf'], stdin = p1.stdout))
+	p1 = subprocess.Popen(['bcftools', 'mpileup', '-r', contig, '-C', '50', '-pm2', '-F', '0.2', '-f', fm_obj.localGenomeFile] + bamfiles, stdout = subprocess.PIPE)
+	processes.append(subprocess.Popen(['bcftools', 'call', '-vmO', 'v', '-f', 'GQ', '-o', contig + '.vcf'], stdin = p1.stdout))
 
 	if len(processes) > 23:
 		for p in processes:	
 			p.communicate()
 		processes = []
 	#timer.stop()
-
+"""
 
