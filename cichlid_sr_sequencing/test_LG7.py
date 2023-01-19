abcfc3fd5060659b01dc551ac64b837f6852f16a
nkumar317 <nkumar317@gatech.edu>
2023-01-19 16:08:45 -0500

    testing interval list issue

--- cichlid_sr_sequencing/test_LG7.py
+++ cichlid_sr_sequencing/test_LG7.py
@@ -19,8 +19,7 @@ if args.Genome not in fm_obj.returnGenomeVersions():
 # fm_obj.downloadData(fm_obj.localGenomeDir)
 
 lg7 = 'NC_036786.1'
-sp.run(shlex.split(f"gatk GenomicsDBImport --genomicsdb-workspace-path {'/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Databases/' + lg7 + '_database'} --intervals lg7.interval_list --sample-name-map lg7_sample_map.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
+sp.run(shlex.split(f"gatk GenomicsDBImport --genomicsdb-workspace-path {'/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Databases/' + lg7 + '_database'} --intervals 4_per_LG.interval_list --sample-name-map lg7_sample_map.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
 # sp.run(shlex.split(f"gatk GenotypeGVCFs -R /Data/mcgrath-lab/Data/CichlidSequencingData/Genomes/Mzebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna -V {'gendb://../../Data/CichlidSequencingData/Databases/' + lg7 + '_database/'}  -O {'/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Outputs/' + lg7 + '_output.vcf'} --heterozygosity 0.0012"))
 
 print('Pipeline Completed')
-# START BY ADDING THE CODE TO DOWNLOAD THE DATA TO AUTOMATICALLY CREATE THE DIR STRUCTURE 
