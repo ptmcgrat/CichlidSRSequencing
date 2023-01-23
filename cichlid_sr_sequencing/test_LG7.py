a98d773abce22bd050bd5892ef7fccd667e2b5d4
nkumar317 <nkumar317@gatech.edu>
2023-01-23 16:28:21 -0500

    updated code to run each sample at a time and record which samples fail

--- cichlid_sr_sequencing/test_LG7.py
+++ cichlid_sr_sequencing/test_LG7.py
@@ -1,5 +1,5 @@
 # for testing on the mzebra server only
-import subprocess as sp, argparse, shlex, pysam, os
+import subprocess as sp, argparse, shlex, pandas as pd
 from helper_modules.file_manager import FileManager as FM
 
 
@@ -18,11 +18,43 @@ if args.Genome not in fm_obj.returnGenomeVersions():
 #### Download the reference genome files and build the proper directry structure for them locally.
 # fm_obj.downloadData(fm_obj.localGenomeDir)
 
+a_dt = pd.read_csv(fm_obj.localAlignmentFile)
+
+a_dt = a_dt[a_dt.GenomeVersion == args.Genome]
+
+sampleIDs = set(a_dt.SampleID)
+#### After a discussion with Patrick, the most efficient and quick way to figure out which samples are not running for LG7 is to simply run each sample one by 1 and store its data into a separate Database. If a database's creation is not working, then on exception, we can record the sample name into a file and pass that one
 lg7 = 'NC_036786.1'
+processes = []
+for sample in sampleIDs:
+	try:
+		p = sp.run(shlex.split(f"gatk --java-options '-Xmx450G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/lg7_Databases/' + sample + '_database'} --intervals lg7.interval_list -V {'/Data/mcgrath-lab/Data/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/' + sample + '/' + sample + '.g.vcf.gz'} --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
+		processes.append(p)
+		if len(processes) == 22:
+			for p in processes:
+				p.communicate()
+			processes = []
+	except:
+		with open('failed_samples.txt', 'w') as fh:
+			fh.write(f"sample {sample} failed GenomicsDBImport")
+		pass
+
+	
+
+# for contig in contigs:
+# 	p = sp.run(shlex.split(f"gatk --java-options '-Xmx100G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/TestingDatabases/' + lg7 + '_database'} --intervals lg7.interval_list --sample-name-map sample_map_utaka.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
+# 	processes2.append(p2)
+
+# 	if len(processes2) == 22:
+# 		for p in processes2:
+# 			p.communicate()
+# 		processes2=[]
+
+
 # sp.run(shlex.split(f"gatk --java-options '-Xmx100G' GenomicsDBImport --genomicsdb-workspace-path {'/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Databases/' + lg7 + '_database'} --intervals lg7.interval_list --sample-name-map lg7_sample_map.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
 # sp.run(shlex.split(f"gatk GenotypeGVCFs -R /Data/mcgrath-lab/Data/CichlidSequencingData/Genomes/Mzebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna -V {'gendb://../../Data/CichlidSequencingData/Databases/' + lg7 + '_database/'}  -O {'/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Outputs/' + lg7 + '_output.vcf'} --heterozygosity 0.0012"))
 
 #### The below command is only to be run on the Utaka server and will run a GenomicsDBImport for all samples for LG7 only. 
-sp.run(shlex.split(f"gatk --java-options '-Xmx100G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/TestingDatabases/' + lg7 + '_database'} --intervals lg7.interval_list --sample-name-map sample_map_utaka.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
+# sp.run(shlex.split(f"gatk --java-options '-Xmx100G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/TestingDatabases/' + lg7 + '_database'} --intervals lg7.interval_list --sample-name-map sample_map_utaka.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
 
 print('Pipeline Completed')
