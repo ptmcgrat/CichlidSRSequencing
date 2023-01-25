cce5c6f3670a262c5d743e82854724f21fa5bcec
nkumar317 <nkumar317@gatech.edu>
2023-01-25 16:14:15 -0500

    uncommented the code to test for which sample is failing

--- cichlid_sr_sequencing/test_LG7.py
+++ cichlid_sr_sequencing/test_LG7.py
@@ -18,30 +18,30 @@ if args.Genome not in fm_obj.returnGenomeVersions():
 #### Download the reference genome files and build the proper directry structure for them locally.
 # fm_obj.downloadData(fm_obj.localGenomeDir)
 
-a_dt = pd.read_csv(fm_obj.localAlignmentFile)
+# a_dt = pd.read_csv(fm_obj.localAlignmentFile)
+# a_dt = a_dt[a_dt.GenomeVersion == args.Genome]
+# sampleIDs = set(a_dt.SampleID)
 
-a_dt = a_dt[a_dt.GenomeVersion == args.Genome]
-
-sampleIDs = set(a_dt.SampleID)
-
+data = pd.read_csv('sample_IDs.csv')
+sampleIDs = data['sample_IDs'].tolist()
 lg7 = 'NC_036786.1'
 #### After a discussion with Patrick, the most efficient and quick way to figure out which samples are not running for LG7 is to simply run each sample one by 1 and store its data into a separate Database. If a database's creation is not working, then on exception, we can record the sample name into a file and pass that one
-# processes = []
-# for sample in sampleIDs:
-# 	try:
-# 		p = sp.run(shlex.split(f"gatk --java-options '-Xmx450G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/lg7_Databases/' + sample + '_database'} --intervals lg7.interval_list -V {'/Data/mcgrath-lab/Data/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/' + sample + '/' + sample + '.g.vcf.gz'} --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
-# 		processes.append(p)
-# 		if len(processes) == 22:
-# 			for p in processes:
-# 				p.communicate()
-# 			processes = []
-# 	except:
-# 		with open('failed_samples.txt', 'a+') as fh:
-# 			fh.write(f"sample {sample} failed GenomicsDBImport\n")
-# 		pass
+processes = []
+for sample in sampleIDs:
+	try:
+		p = sp.run(shlex.split(f"gatk --java-options '-Xmx450G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/TestingDatabases/' + sample + '_database'} --intervals lg7.interval_list -V {'/Data/mcgrath-lab/Data/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/' + sample + '/' + sample + '.g.vcf.gz'} --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
+		processes.append(p)
+		if len(processes) == 22:
+			for p in processes:
+				p.communicate()
+			processes = []
+	except:
+		with open('failed_samples.txt', 'a+') as fh:
+			fh.write(f"sample {sample} failed GenomicsDBImport\n")
+		pass
 
 #### The above code was used to ID the failed sample (SAMEA4033252). The below code is to create an LG7 database for the samples including the SAMEA4033252 and create a VCF file once the GenomicsDBImport is complete
-sp.run(shlex.split(f"gatk --java-options '-Xmx450G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/Databases/' + lg7 + '_database'} --intervals lg7.interval_list --sample-name-map sample_map_utaka.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
-sp.run(shlex.split(f"gatk --java-options '-Xmx450G' GenotypeGVCFs -R /Data/mcgrath-lab/Data/CichlidSequencingData/Genomes/Mzebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna -V {'gendb://../../../../../Data/mcgrath-lab/Data/CichlidSequencingData/Databases/' + lg7 + '_database/'}  -O {'/Data/mcgrath-lab/Data/CichlidSequencingData/Outputs/' + lg7 + '_output.vcf'} --heterozygosity 0.0012"))
+# sp.run(shlex.split(f"gatk --java-options '-Xmx450G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/Databases/' + lg7 + '_database'} --intervals lg7.interval_list --sample-name-map sample_map_utaka.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
+# sp.run(shlex.split(f"gatk --java-options '-Xmx450G' GenotypeGVCFs -R /Data/mcgrath-lab/Data/CichlidSequencingData/Genomes/Mzebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna -V {'gendb://../../../../../Data/mcgrath-lab/Data/CichlidSequencingData/Databases/' + lg7 + '_database/'}  -O {'/Data/mcgrath-lab/Data/CichlidSequencingData/Outputs/' + lg7 + '_output.vcf'} --heterozygosity 0.0012"))
 
-print('Pipeline Completed')
+# print('Pipeline Completed')
