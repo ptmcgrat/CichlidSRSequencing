6ce5d968388e76299d761c3ce45e4eb92b3a9a58
nkumar317 <nkumar317@gatech.edu>
2023-02-03 12:49:54 -0500

    parallel download test on mzebra

--- cichlid_sr_sequencing/haplotypecaller_lg7.py
+++ cichlid_sr_sequencing/haplotypecaller_lg7.py
@@ -1,4 +1,4 @@
-import subprocess as sp, argparse, shlex, pandas as pd, pysam, os
+import subprocess as sp, argparse, shlex
 from helper_modules.file_manager import FileManager as FM
 
 
@@ -29,7 +29,7 @@ processes = []
 for file in error_files:
 	p = sp.Popen(shlex.split(f"rclone copy ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/{file}/{file}.all.bam /home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/{file}/ -P"))
 	processes.append(p)
-	if len(processes) == 74:
+	if len(processes) == 5:
 		for p in processes:
 			p.communicate()
 		processes = []
