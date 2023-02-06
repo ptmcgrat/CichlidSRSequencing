7b154d34539e8267cd1a3cc9ae403b4ba4c12daa
nkumar317 <nkumar317@gatech.edu>
2023-02-05 20:38:48 -0500

    fixed to filter using DP and display correct filtering and PASS numbers

--- cichlid_sr_sequencing/filter_variants.py
+++ cichlid_sr_sequencing/filter_variants.py
@@ -21,16 +21,17 @@ args = parser.parse_args()
 #    --filter-name 'depth_Qual' \
 #    --filter-expression 'QD < 2.0' \
 #    --filter-name 'max_DP' \
-#    --filter-expression 'DP > 3000.0' \
+#    --filter-expression 'DP > 3000' \
 #    --filter-name 'min_DP' \
-#    --filter-expression 'DP < 1000.0' \
+#    --filter-expression 'DP < 1000' \
 #    --filter-name 'strand_bias' \
 #    --filter-expression 'FS > 40.0' \
 #    --filter-name 'mapping_quality' \
 #    --filter-expression 'MQ < 40.0' \
 #    --filter-name 'no_calls' \
-#    --filter-expression 'NCC > 125.0' \"))
-
+#    --filter-expression 'NCC > 125.0' \
+#    --verbosity ERROR"))
+# sp.run(shlex.split(f'gunzip {out}'))
 
 ### Mzebra server File paths
 vcf = '/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Outputs/vcf_concat_output/original_data/master_file.vcf.gz'
@@ -58,10 +59,10 @@ sp.run(shlex.split(f"gatk VariantFiltration \
    --filter-name 'no_calls' \
    --filter-expression 'NCC > 125.0' \
    --verbosity ERROR"))
-
+sp.run(shlex.split(f'gunzip {out}'))
 
 #### test local filter file for number of filters called in each variant
-# filters = {'allele_freq':0, 'inbreeding_test':0, 'depth_Qual':0, 'max_DP':0, 'min_DP':0, 'strand_bias':0, 'mapping_quality':0, 'no_calls':0}
+# filters = {'allele_freq':0, 'inbreeding_test':0, 'depth_Qual':0, 'max_DP':0, 'min_DP':0, 'strand_bias':0, 'mapping_quality':0, 'no_calls':0, 'PASS':0}
 
 # with open('/Users/kmnike/Data/CichlidSequencingData/Outputs/test_out.vcf', 'r') as fh:
 #    for line in fh:
@@ -71,21 +72,20 @@ sp.run(shlex.split(f"gatk VariantFiltration \
 #       filter = filter.split(';')
 #       for criteria in filter:
 #          filters[criteria] += 1
-      
 # print(filters)
 
 
 
 #### see how many filters were triggered for variants in the whole file. filters = {'allele_freq':0, 'inbreeding_test':0, 'depth_Qual':0, 'max_DP':0, 'min_DP':0, 'strand_bias':0, 'mapping_quality':0, 'no_calls':0}
 
-# filters = {'allele_freq':0, 'inbreeding_test':0, 'depth_Qual':0, 'max_DP':0, 'min_DP':0, 'strand_bias':0, 'mapping_quality':0, 'no_calls':0, 'PASS':0}
-# with open('/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Outputs/vcf_concat_output/master_file_filtered.vcf', 'r') as fh:
-#    for line in fh:
-#       if line.strip().split()[0].startswith('##') or line.strip().split()[0].startswith('#'):
-#          continue
-#       filter = line.split('\t')[6]
-#       filter = filter.split(';')
-#       for criteria in filter:
-#          filters[criteria] += 1
+filters = {'allele_freq':0, 'inbreeding_test':0, 'depth_Qual':0, 'max_DP':0, 'min_DP':0, 'strand_bias':0, 'mapping_quality':0, 'no_calls':0, 'PASS':0}
+with open('/home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Outputs/vcf_concat_output/master_file_correctly_filtered.vcf', 'r') as fh:
+   for line in fh:
+      if line.strip().split()[0].startswith('##') or line.strip().split()[0].startswith('#'):
+         continue
+      filter = line.split('\t')[6]
+      filter = filter.split(';')
+      for criteria in filter:
+         filters[criteria] += 1
    
-# print(filters)
+print(filters)
