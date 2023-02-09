0bc2e0cb3593fb592b7cf1854ddb0ed189478be1
nkumar317 <nkumar317@gatech.edu>
2023-02-09 15:04:50 -0500

    updated code to process each unmapped contig individually on 4 cores each

--- cichlid_sr_sequencing/run_unmapped_contigs.py
+++ cichlid_sr_sequencing/run_unmapped_contigs.py
@@ -4,23 +4,18 @@ from helper_modules.Timer import Timer
 import shlex
 import pandas as pd
 
-parser = argparse.ArgumentParser(usage = 'This script is for troubleshooting why LGs 3, 7, 15, and 20 failed to run ')
-parser.add_argument('Genome', type = str, help = 'Version of the genome to align to')
+parser = argparse.ArgumentParser(usage = 'This script is for running unmapped contigs in Mzebra_UMD2a')
+# parser.add_argument('Genome', type = str, help = 'Version of the genome to align to')
 args = parser.parse_args()
 
 # Create FileManager object to keep track of filenames
-fm_obj = FM(args.Genome)
+genome = 'Mzebra_UMD2a'
+fm_obj = FM(genome)
 
 # Make sure genome version is a valid option
-if args.Genome not in fm_obj.returnGenomeVersions():
-	raise argparse.ArgumentTypeError('Genome version does not exist. Options are: ' + ','.join(fm_obj.returnGenomeVersions()))
-
-fm_obj.downloadData(fm_obj.localAlignmentFile)
 
 a_dt = pd.read_csv(fm_obj.localAlignmentFile)
-
-a_dt = a_dt[a_dt.GenomeVersion == args.Genome]
-
+a_dt = a_dt[a_dt.GenomeVersion == genome]
 sampleIDs = set(a_dt.SampleID)
 
 gvcffiles = []
@@ -28,23 +23,27 @@ gvcffiles = []
 # Download the reference genome files and build the proper directry structure for them locally.
 # fm_obj.downloadData(fm_obj.localGenomeDir)
 fasta_obj = pysam.FastaFile(fm_obj.localGenomeFile)
+contigs = fasta_obj.references[22:]
 
-# Download the GVCF and GVCF.idx files for each sample in the AlignmentDatabase
-# for sampleID in sampleIDs:
-# 	if sampleID in []:
-# 		continue
-# 	print(sampleID)
-# 	fm_obj.createSampleFiles(sampleID)
-# 	fm_obj.downloadData(fm_obj.localGVCFFile)
-# 	fm_obj.downloadData(fm_obj.localGVCFFile + '.tbi')
-# 	gvcffiles.append(fm_obj.localGVCFFile)
-	# pdb.set_trace() # used for troubleshooting
-
-processes = []
+processes1 = []
 processes2 = []
 
-#### Since the intervals list will generate one database for all of the unmapped contigs, I can run GenomcisDBImport and GenotypeGVCFs back to back without the need to parallelize further (since it's not possible to parallelize GenotypeGVCFs this way anyways)
-sp.run(shlex.split(f"gatk GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/Databases/' + 'all_unmapped_contigs_database'} --intervals unmapped_contigs.interval_list --sample-name-map sample_map_utaka.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
-sp.run(shlex.split(f"gatk GenotypeGVCFs -R /Data/mcgrath-lab/Data/CichlidSequencingData/Genomes/Mzebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna -V {'gendb://../../../../../Data/mcgrath-lab/Data/CichlidSequencingData/Databases/all_unmapped_contigs_database'}  -O {'/Data/mcgrath-lab/Data/CichlidSequencingData/Outputs/' + 'all_unmapped_contigs_output.vcf'} --heterozygosity 0.0012"))
+#### Process unmapped contigs 22 at a time through GenomicsDBImport. I'm giving each process 20Gb --> 440Gb memory being used per 22 processes
+for contig in contigs:
+	p1 = sp.Popen(shlex.split(f"gatk --java-options '-Xmx20G' GenomicsDBImport --genomicsdb-workspace-path {'/Data/mcgrath-lab/Data/CichlidSequencingData/Databases/' + contig + '_database'} --intervals {'/home/mcgrath-lab/nkumar317/CichlidSRSequencing/cichlid_sr_sequencing/intervals_unmapped_contigs/' + contig + '.interval_list'} --sample-name-map sample_map_utaka.txt --interval-merging-rule OVERLAPPING_ONLY --max-num-intervals-to-import-in-parallel 4 --overwrite-existing-genomicsdb-workspace"))
+	processes1.append(p1)
+	for proc in processes1:
+		if len(processes1) == 22:
+			proc.communicate()
+	processes1 = []
 
+
+for contig in contigs:
+	p2 = sp.Popen(shlex.split(f"gatk --java-options '-Xmx20G' GenotypeGVCFs -R /Data/mcgrath-lab/Data/CichlidSequencingData/Genomes/Mzebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna -V {'gendb://../../../../../Data/mcgrath-lab/Data/CichlidSequencingData/Databases/' + contig + '_database'}  -O {'/Data/mcgrath-lab/Data/CichlidSequencingData/Outputs/' + contig + '_output'} --heterozygosity 0.0012"))
+	processes2.append(p2)
+	for proc in processes2:
+		if len(processes2) == 22:
+			proc.communicate()
+	processes2 = []
+	
 print('Pipeline Completed')
