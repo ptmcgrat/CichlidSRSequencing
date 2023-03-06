015243306a26b5e0f098a123ea385fad3c91c900
nkumar317 <nkumar317@gatech.edu>
2023-03-06 00:20:53 -0500

    split lg11 into intervals for PCA

--- cichlid_sr_sequencing/analysis_pipeline/intervals_lg11/make_lg11_intervals_files.py
+++ cichlid_sr_sequencing/analysis_pipeline/intervals_lg11/make_lg11_intervals_files.py
@@ -5,5 +5,5 @@ regions  = ['pre_inversion', 'inversion1', 'inter_inversion', 'inversion2', 'pos
 
 for region in regions:
     # print(f"/Users/kmnike/CichlidSRSequencing/cichlid_sr_sequencing/analysis_pipeline/intervals_lg11/{region}.interval_list")
-    sp.run(shlex.split(f"gatk SelectVariants -V /Users/kmnike/CichlidSRSequencing/Test/samples_filtered_master.vcf.gz -L /Users/kmnike/CichlidSRSequencing/cichlid_sr_sequencing/analysis_pipeline/intervals_lg11/{region}.interval_list -O {region}.vcf"))
+    sp.run(shlex.split(f"gatk SelectVariants -V /home/ad.gatech.edu/bio-mcgrath-dropbox/Data/CichlidSequencingData/Outputs/vcf_concat_output/pipeline_outputs/samples_filtered_master.vcf.gz -L /home/ad.gatech.edu/bio-mcgrath-dropbox/CichlidSRSequencing/cichlid_sr_sequencing/analysis_pipeline/intervals_lg11/{region}.interval_list -O {region}.vcf"))
     sp.run(shlex.split(f"bgzip {region}.vcf"))
