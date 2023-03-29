2a085829b8219207c390ef6f501d7f38f9e7d776
nkumar317 <nkumar317@gatech.edu>
2023-03-29 14:36:26 -0400

    download bams

--- cichlid_sr_sequencing/analysis_pipeline/download_bams.py
+++ cichlid_sr_sequencing/analysis_pipeline/download_bams.py
@@ -5,5 +5,5 @@ fm_obj = FM('Mzebra_UMD2a')
 samples = ['MC_1_m', 'SAMEA2661294', 'SAMEA2661322', 'SAMEA4032100', 'SAMEA4033261']
 for sample in samples:
     fm_obj.createSampleFiles(sample)
-    sp.run(shlex.split(f'rclone copy ptm_dropbox:{fm_obj.locaBamFile} .'))
-    sp.run(shlex.split(f'rclone copy ptm_dropbox:{fm_obj.locaBamIndexFile} .'))
+    sp.run(shlex.split(f'rclone copy ptm_dropbox:{fm_obj.localBamFile} .'))
+    sp.run(shlex.split(f'rclone copy ptm_dropbox:{fm_obj.localBamIndexFile} .'))
