562996c3a4fa368c48fe685a0a51b7be2585f978
nkumar317 <nkumar317@gatech.edu>
2023-03-29 14:41:45 -0400

    download bams

--- cichlid_sr_sequencing/analysis_pipeline/download_bams.py
+++ cichlid_sr_sequencing/analysis_pipeline/download_bams.py
@@ -5,5 +5,5 @@ fm_obj = FM('Mzebra_UMD2a')
 samples = ['MC_1_m', 'SAMEA2661294', 'SAMEA2661322', 'SAMEA4032100', 'SAMEA4033261']
 for sample in samples:
     fm_obj.createSampleFiles(sample)
-    sp.run(shlex.split(f'rclone copy ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/{sample}/{sample}.all.bam ~/bams'))
-    sp.run(shlex.split(f'rclone copy ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/{sample}/{sample}.bai ~/bams'))
+    sp.run(shlex.split(f'rclone copy ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/{sample}/{sample}.all.bam /home/mcgrath-lab/bams'))
+    sp.run(shlex.split(f'rclone copy ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Bamfiles/Mzebra_UMD2a/{sample}/{sample}.bai /home/mcgrath-lab/bams'))
