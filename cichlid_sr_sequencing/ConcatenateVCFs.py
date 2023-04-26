2c1645a2e6a6c40449b0bd05d96ac35dba10c16b
nkumar317 <nkumar317@gatech.edu>
2023-04-26 11:03:11 -0400

    updated concat script for 576 sample cohort

--- cichlid_sr_sequencing/ConcatenateVCFs.py
+++ cichlid_sr_sequencing/ConcatenateVCFs.py
@@ -10,7 +10,7 @@ fm_obj = FM(args.Genome)
 
 #### use rclone lsf to create a list of existing vcf files 
 outputs = sp.run(shlex.split("rclone lsf ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Outputs --include '*.vcf'"), stdout=sp.PIPE, encoding='utf-8').stdout.splitlines()
-path_to_outputs = 'ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Outputs/'
+path_to_outputs = 'ptm_dropbox:BioSci-McGrath/Apps/CichlidSequencingData/Outputs/Mzebra_UMD2a/Outputs576Cohort/'
 
 #### check if an output dir exists. If not, make it.
 try:
