99fa4f44f8d4d6a67c6cf7e7ec02eb9f8187001b
Nikesh Kumar <kmnike@ipsec-10-2-192-144.vpn.gatech.edu>
2024-02-02 15:25:15 -0500

    updated GT1 reference to Mzebra_GT2.fna

--- cichlid_sr_sequencing/analysis_pipeline/helper_modules/nikesh_file_manager.py
+++ cichlid_sr_sequencing/analysis_pipeline/helper_modules/nikesh_file_manager.py
@@ -61,7 +61,7 @@ class FileManager():
 		if self.genome_version == 'Mzebra_UMD2a':
 			self.localGenomeFile = self.localGenomeDir + 'GCF_000238955.4_M_zebra_UMD2a_genomic.fna'
 		elif self.genome_version == 'Mzebra_GT1':
-			self.localGenomeFile = self.localGenomeDir + 'Mzebra_GT1_v1.fna'
+			self.localGenomeFile = self.localGenomeDir + 'Mzebra_GT2.fna'
 		
 
 		self.localSampleFile = self.localReadsDir + 'SampleDatabase.csv'
