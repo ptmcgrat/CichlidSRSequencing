e0d58797247a3ec02fc9b060aba97addacf6b50f
ptmcgrat <pmcgrath7@gatech.edu>
2024-03-20 13:27:27 -0400

    New Remote

--- cichlid_sr_sequencing/analyzeGenomeSize.py
+++ cichlid_sr_sequencing/analyzeGenomeSize.py
@@ -11,8 +11,8 @@ def process_sample(sampleID, fm_obj):
 	#for raw_bam in fm_obj.localRawBamFiles:
 	#	subprocess.run(['samtools','fastq',raw_bam], stdout = open(fm_obj.localReadsDir + sampleID + '.fq', 'a'))
 
-	subprocess.run(['jellyfish','count','-m','21','-s','6G', '-C', '-t', '96', '-o', fm_obj.localReadsDir + sampleID + '_counts.jf', fm_obj.localReadsDir + sampleID + '.fq'])
-	subprocess.run(['jellyfish','histo','-t','-96',fm_obj.localReadsDir + sampleID + '_counts.jf'], stdout = open(fm_obj.localReadsDir + sampleID + '.histo', 'w'))
+	#subprocess.run(['jellyfish','count','-m','21','-s','6G', '-C', '-t', '96', '-o', fm_obj.localReadsDir + sampleID + '_counts.jf', fm_obj.localReadsDir + sampleID + '.fq'])
+	subprocess.run(['jellyfish','histo','-t','96',fm_obj.localReadsDir + sampleID + '_counts.jf'], stdout = open(fm_obj.localReadsDir + sampleID + '.histo', 'w'))
 	#jellyfish histo -t 10 reads.jf > reads.histo
 	
 
