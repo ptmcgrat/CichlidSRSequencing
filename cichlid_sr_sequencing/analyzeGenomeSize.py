2c4bd85884df31aceccbbb2e98ff510ab1d55e94
ptmcgrat <pmcgrath7@gatech.edu>
2024-03-20 13:43:10 -0400

    New Remote

--- cichlid_sr_sequencing/analyzeGenomeSize.py
+++ cichlid_sr_sequencing/analyzeGenomeSize.py
@@ -5,23 +5,22 @@ import pdb, subprocess, sys
 def process_sample(sampleID, fm_obj):
 	fm_obj.createSampleFiles(sampleID)
 
-	#for raw_bam in fm_obj.localRawBamFiles:
-	#	fm_obj.downloadData(raw_bam)
+	for raw_bam in fm_obj.localRawBamFiles:
+		fm_obj.downloadData(raw_bam)
 
-	#for raw_bam in fm_obj.localRawBamFiles:
-	#	subprocess.run(['samtools','fastq',raw_bam], stdout = open(fm_obj.localReadsDir + sampleID + '.fq', 'a'))
+	for raw_bam in fm_obj.localRawBamFiles:
+		subprocess.run(['samtools','fastq',raw_bam], stdout = open(fm_obj.localReadsDir + sampleID + '.fq', 'a'))
 
-	#subprocess.run(['jellyfish','count','-m','21','-s','6G', '-C', '-t', '96', '-o', fm_obj.localReadsDir + sampleID + '_counts.jf', fm_obj.localReadsDir + sampleID + '.fq'])
+	subprocess.run(['jellyfish','count','-m','21','-s','6G', '-C', '-t', '96', '-o', fm_obj.localReadsDir + sampleID + '_counts.jf', fm_obj.localReadsDir + sampleID + '.fq'])
 	subprocess.run(['jellyfish','histo','-t','96',fm_obj.localReadsDir + sampleID + '_counts.jf'], stdout = open(fm_obj.localReadsDir + sampleID + '.histo', 'w'))
-	#jellyfish histo -t 10 reads.jf > reads.histo
-	
+	subprocess.run(['rm','-f', fm_obj.localReadsDir + sampleID + '.fq', fm_obj.localReadsDir + sampleID + '_counts.jf'] + fm_obj.localRawBamFiles)
 
 # Create fm_obj and grab sample file
 fm_obj = FM(genome_version = 'Mzebra_UMD2a')
 
 
-
-process_sample('MZ_1_m', fm_obj)
+for sample in ['YH_1_m','CV_1_m','CV_2_f','MZ_1_m','MZ_2_f','MC_1_m','MC_2_f']
+process_sample(sample, fm_obj)
 
 
 """
