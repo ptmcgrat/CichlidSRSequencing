5f497e88fd4c1e03bb0660b14e09649b7f53a93d
ptmcgrat <pmcgrath7@gatech.edu>
2024-03-20 10:30:57 -0400

    New Remote

--- cichlid_sr_sequencing/analyzeGenomeSize.py
+++ cichlid_sr_sequencing/analyzeGenomeSize.py
@@ -2,16 +2,27 @@ from helper_modules.file_manager import FileManager as FM
 import pandas as pd 
 import pdb, subprocess, sys
 
+def process_sample(sampleID, fm_obj):
+	fm_obj.createSampleFiles(sampleID)
+
+	for raw_bam in fm_obj.localRawBamFiles:
+		fm_obj.downloadData(raw_bam)
+
+	for raw_bam in fm_obj.localRawBamFiles:
+		subprocess.run(['samtools','fastq',raw_bam], stdout = open(fm_obj.localReadsDir + sampleID + '.fq', 'a'))
+
+	subprocess.run(['jellyfish','count','-m','21','-s','3G', '-C', '-o', fm_obj.localReadsDir + sampleID + '_counts.jf', fm_obj.localReadsDir + sampleID + '.fq'])
+	subprocess.run(['jellyfish','histo','-t','-20',fm_obj.localReadsDir + sampleID + '_counts.jf'], stdout = open(fm_obj.localReadsDir + sampleID + '.histo'))
+	#jellyfish histo -t 10 reads.jf > reads.histo
+	
+
 # Create fm_obj and grab sample file
 fm_obj = FM(genome_version = 'Mzebra_UMD2a')
 
-fm_obj.createSampleFiles('MZ_1_m')
 
-for raw_bam in fm_obj.localRawBamFiles:
-	fm_obj.downloadData(raw_bam)
 
-for raw_bam in fm_obj.localRawBamFiles:
-	subprocess.run(['samtools','fastq',raw_bam], stdout = open(fm_obj.localReadsDir + 'MZ_1_m.fq', 'a'))
+process_sample('MZ_1_m', fm_obj)
+
 
 """
 YH_1_m
