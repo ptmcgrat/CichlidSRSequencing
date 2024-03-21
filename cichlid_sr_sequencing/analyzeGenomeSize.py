9ff90f87cc521d7de7600d1a7f7c2036b38862d1
McGrath <pmcgrath7@ipsec-10-2-228-46.vpn.gatech.edu>
2024-03-21 10:49:19 -0400

    Fix Fastq files

--- cichlid_sr_sequencing/analyzeGenomeSize.py
+++ cichlid_sr_sequencing/analyzeGenomeSize.py
@@ -11,6 +11,7 @@ def process_sample(sampleID, fm_obj):
 	for raw_bam in fm_obj.localRawBamFiles:
 		subprocess.run(['samtools','fastq',raw_bam], stdout = open(fm_obj.localReadsDir + sampleID + '.fq', 'a'))
 
+	subprocess.run(['fastqc', fm_obj.localReadsDir + sampleID + '.fq'])
 	subprocess.run(['jellyfish','count','-m','21','-s','6G', '-C', '-t', '96', '-o', fm_obj.localReadsDir + sampleID + '_counts.jf', fm_obj.localReadsDir + sampleID + '.fq'])
 	subprocess.run(['jellyfish','histo','-t','96',fm_obj.localReadsDir + sampleID + '_counts.jf'], stdout = open(fm_obj.localReadsDir + sampleID + '.histo', 'w'))
 	subprocess.run(['rm','-f', fm_obj.localReadsDir + sampleID + '.fq', fm_obj.localReadsDir + sampleID + '_counts.jf'] + fm_obj.localRawBamFiles)
