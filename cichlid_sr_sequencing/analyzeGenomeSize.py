5758fcd86e86593b9d6733e6fb9fb4ba65c8dbb0
McGrath <pmcgrath7@ipsec-10-2-98-211.vpn.gatech.edu>
2024-07-17 18:43:01 -0400

    New Genome alignment

--- cichlid_sr_sequencing/analyzeGenomeSize.py
+++ cichlid_sr_sequencing/analyzeGenomeSize.py
@@ -5,7 +5,7 @@ import pdb, subprocess, sys
 fm_obj = FM(genome_version = 'Mzebra_GT3')
 fm_obj.downloadData(fm_obj.localGenomeFile)
 
-soft_dir = '/home/ad.gatech.edu/bio-mcgrath-dropbox/Patrick/RepeatAnnotation/RepeatModeler-2.0.5'
+soft_dir = '/home/ad.gatech.edu/bio-mcgrath-dropbox/Patrick/RepeatAnnotation/RepeatModeler-2.0.5/'
 
 subprocess.run([soft_dir + 'BuildDatabase','-name',fm_obj.localGenomeDir + 'Mzebra_GT3', fm_obj.localGenomeFile])
 subprocess.run([soft_dir + 'RepeatModeler','-database', 'Mzebra_GT3','-threads','12','-LTRStruct'])
