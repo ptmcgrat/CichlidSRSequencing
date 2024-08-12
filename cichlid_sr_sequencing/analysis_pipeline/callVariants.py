c0d17d7cd1152efeb6f894e9d0054b3ce7bb813c
Nikesh Kumar <kmnike@ipsec-10-2-192-88.vpn.gatech.edu>
2024-08-12 16:42:59 -0400

    download gvcf code update to download to /Outputs on Utaka

--- cichlid_sr_sequencing/analysis_pipeline/callVariants.py
+++ cichlid_sr_sequencing/analysis_pipeline/callVariants.py
@@ -118,7 +118,6 @@ class VariantCaller:
     def GVCF_downloader(self, sampleID):
         # Download the GVCF and GVCF.idx files for each sample in self.sampleIDs
         self.fm_obj.createSampleFiles(sampleID)
-        pdb.set_trace()
         if args.local_test: # for downloading files locally and testing if the script works 
             if pathlib.Path(self.fm_obj.localTestGVCFFile).exists():
                 print('GCVF file for ' + sampleID + ' exists. Skipping sample...')
@@ -378,12 +377,12 @@ class VariantCaller:
         elif args.all_sites and args.local_test:
             self.concurrent_processes = 5
         concurrent_processes = min(self.concurrent_processes, len(inputs))
-
         try:
             with multiprocessing.Pool(processes=concurrent_processes) as pool:
                 pool.map(function, inputs)
         except Exception as e:
             print(f"Error occurred during multiprocessing: {e}")
+        pdb.set_trace()
 
     def run_methods(self):
         self._generate_sample_map()
