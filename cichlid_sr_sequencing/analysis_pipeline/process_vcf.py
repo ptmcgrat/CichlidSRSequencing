92ce4d143218ac4a753b4b8cf5b639a563d9daa7
Nikesh Kumar <kmnike@ipsec-10-2-192-88.vpn.gatech.edu>
2024-08-13 10:34:21 -0400

    got rid of unzipped master file stuff

--- cichlid_sr_sequencing/analysis_pipeline/process_vcf.py
+++ cichlid_sr_sequencing/analysis_pipeline/process_vcf.py
@@ -31,7 +31,7 @@ class VCFProcessor:
     def __init__(self, genome):
         self.genome = genome
         self.fm_obj = FM(self.genome)
-        self.master_file = self.fm_obj.localOutputDir + 'phylogenyfigure_master_file.vcf.gz'
+        self.master_file = self.fm_obj.localOutputDir + 'vcf_concat_output/phylogenyfigure_master_file.vcf.gz'
         self.stats_dir = self.fm_obj.localOutputDir + 'filtering_stats/'
         intervals = list(range(1,97))
         self.intervals = list(map(str, intervals))
