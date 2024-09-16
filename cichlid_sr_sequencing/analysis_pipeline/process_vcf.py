ae3767a79e4815a2ddcc0d0080e8f3c20de92409
Nikesh Kumar <kmnike@ipsec-10-2-65-134.vpn.gatech.edu>
2024-09-16 14:21:05 -0400

    for filtering the 498_cohort file into pass_variants file

--- cichlid_sr_sequencing/analysis_pipeline/process_vcf.py
+++ cichlid_sr_sequencing/analysis_pipeline/process_vcf.py
@@ -133,8 +133,8 @@ class VCFProcessor:
 
     def filter_variants(self):
         print('RUNNING VARIANT FILTERING')
-        self.filtered_file = self.fm_obj.localOutputDir + 'vcf_concat_output/CVAnalysis_filtered_master_file.vcf.gz'
-        self.pass_file = self.fm_obj.localOutputDir + 'vcf_concat_output/CVAnalysis_pass_variants_master_file.vcf.gz'
+        self.filtered_file = self.fm_obj.localOutputDir + 'vcf_concat_output/498_cohort_filtered_master_file.vcf.gz'
+        self.pass_file = self.fm_obj.localOutputDir + 'vcf_concat_output/498_cohort_pass_variants_master_file.vcf.gz'
         subprocess.run(shlex.split(f"gatk VariantFiltration \
                                     -R {self.fm_obj.localGenomeFile} \
                                     -V {self.master_file} \
@@ -146,15 +146,15 @@ class VCFProcessor:
                                     --filter-name 'depth_Qual' \
                                     --filter-expression 'QD < 2.0' \
                                     --filter-name 'max_DP' \
-                                    --filter-expression 'DP > 425' \
+                                    --filter-expression 'DP > 11000' \
                                     --filter-name 'min_DP' \
-                                    --filter-expression 'DP < 200' \
+                                    --filter-expression 'DP < 7600' \
                                     --filter-name 'strand_bias' \
                                     --filter-expression 'FS > 40.0' \
                                     --filter-name 'mapping_quality' \
                                     --filter-expression 'MQ < 50.0' \
                                     --filter-name 'no_calls' \
-                                    --filter-expression 'NCC > 7' \
+                                    --filter-expression 'NCC > 119' \
                                     --verbosity ERROR"))
         print('FILTERING COMPLETE... EXTRACTING ONLY PASS VARIANTS')
         subprocess.run(['gatk', 'SelectVariants', '-V', self.filtered_file, '--exclude-filtered', '-O', self.pass_file])
@@ -194,5 +194,5 @@ time python process_vcf.py Mzebra_GT3 --merge --compress_and_index
 
 time python process_vcf.py Mzebra_GT3 --filter 2> error_filter_gt3_master_file_240723.txt 1> log_filter_gt3_master_file_240723.txt
 
-
+time python process_vcf.py Mzebra_GT3 --filter
 """
