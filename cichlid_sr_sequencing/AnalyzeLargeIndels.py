6865deaef8b5c146e3a058f58bae91d760024ece
Patrick McGrath <pmcgrath7@ipsec-10-2-225-227.vpn.gatech.edu>
2026-03-27 16:01:27 -0400

    Small updates

--- cichlid_sr_sequencing/AnalyzeLargeIndels.py
+++ cichlid_sr_sequencing/AnalyzeLargeIndels.py
@@ -7,8 +7,9 @@ fm_obj = FM(genome_version = 'Mzebra_GT3')
 
 insertions = [24869679,24918628,24921178,24921394]
 male_yhs = ['YH_011_m','YH_008_m','YH_1_m','YH_005_m','YH_003_m','YH_025','YH_026','YH_027','YH_028','YH_029','YH_030','YH_037','YH_038','YH_039','YH_040','YH_041','YH_042','YH_046','YH_047','YH_048','YH_049','YH_050','YH_051','YH_052','YH_053','YH_054','YH_068','YH_069','YH_070','YH_012_m','YH_014_m','Kocher_YH1m','YH_015_m']
+female_yhs = ['Kocher_YH2f','YH_007_f','YH_006_f','YH_013_f','YH_016','YH_017','YH_018','YH_019','YH_020','YH_021','YH_022','YH_023','YH_024','YH_031','YH_032','YH_033','YH_055','YH_056','YH_057','YH_058','YH_059','YH_060','YH_061','YH_062','YH_063','YH_064','YH_065','YH_066','YH_010_f']
 
-out_dt = pd.DataFrame(columns = ['InsertionLocation','Sample','AlnChrom','AlnStart','AlnEnd','PairChrom','PairStart','TemplateLength','Duplicate'])
+out_dt = pd.DataFrame(columns = ['InsertionLocation','Sex','Sample','AlnChrom','AlnStart','AlnEnd','PairChrom','PairStart','TemplateLength','Duplicate'])
 
 for sample in male_yhs:
 	fm_obj.createSampleFiles(sample)
@@ -19,7 +20,20 @@ for sample in male_yhs:
 	for insertion in insertions:
 		for aln in bam_obj.fetch('NC_036789.1',insertion - 2000,insertion+2000):
 			if not aln.is_proper_pair:
-				out = [str(insertion), sample, aln.reference_name, aln.reference_start, aln.reference_end, aln.next_reference_name, aln.next_reference_start, aln.template_length, aln.is_duplicate]
+				out = [str(insertion), 'YHMale', sample, aln.reference_name, aln.reference_start, aln.reference_end, aln.next_reference_name, aln.next_reference_start, aln.template_length, aln.is_duplicate]
 				out_dt.loc[len(out_dt)] = out
+
+for sample in female_yhs:
+	fm_obj.createSampleFiles(sample)
+	fm_obj.downloadData(fm_obj.localDiscordantBamFile)
+	pysam.index(fm_obj.localDiscordantBamFile)
+	bam_obj = pysam.AlignmentFile(fm_obj.localDiscordantBamFile)
+
+	for insertion in insertions:
+		for aln in bam_obj.fetch('NC_036789.1',insertion - 2000,insertion+2000):
+			if not aln.is_proper_pair:
+				out = [str(insertion), 'YHFemale', sample, aln.reference_name, aln.reference_start, aln.reference_end, aln.next_reference_name, aln.next_reference_start, aln.template_length, aln.is_duplicate]
+				out_dt.loc[len(out_dt)] = out
+
 
 out_dt.to_csv('InsertionReadsInYHMales.csv')
