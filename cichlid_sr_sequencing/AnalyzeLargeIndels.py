9ade4b24ef67eaddc5819bac4f590e7e5cd275f1
Patrick McGrath <pmcgrath7@ipsec-10-2-225-227.vpn.gatech.edu>
2026-03-27 13:07:50 -0400

    Small updates

--- cichlid_sr_sequencing/AnalyzeLargeIndels.py
+++ cichlid_sr_sequencing/AnalyzeLargeIndels.py
@@ -8,7 +8,7 @@ fm_obj = FM(genome_version = 'Mzebra_GT3')
 insertions = [24869679,24918628,24921178,24921394]
 male_yhs = ['YH_011_m','YH_008_m','YH_1_m','YH_005_m','YH_003_m','YH_025','YH_026','YH_027','YH_028','YH_029','YH_030','YH_037','YH_038','YH_039','YH_040','YH_041','YH_042','YH_046','YH_047','YH_048','YH_049','YH_050','YH_051','YH_052','YH_053','YH_054','YH_068','YH_069','YH_070','YH_012_m','YH_014_m','Kocher_YH1m','YH_015_m']
 
-out_dt = pd.DataFrame(columns = ['InsertionLocation','AlnChrom','AlnStart','AlnEnd','PairChrom','PairStart','TemplateLength'])
+out_dt = pd.DataFrame(columns = ['InsertionLocation','Sample','AlnChrom','AlnStart','AlnEnd','PairChrom','PairStart','TemplateLength'])
 
 for sample in male_yhs:
 	fm_obj.createSampleFiles(sample)
@@ -17,8 +17,8 @@ for sample in male_yhs:
 	bam_obj = pysam.AlignmentFile(fm_obj.localDiscordantBamFile)
 
 	for insertion in insertions:
-		for aln in aln_obj.fetch('NC_036789.1',insertion - 2000,insertion+2000):
+		for aln in bam_obj.fetch('NC_036789.1',insertion - 2000,insertion+2000):
 			if not aln.is_proper_pair:
-				out = [str(insertion), aln.reference_name, aln.reference_start, aln.reference_end, aln.next_reference_name, aln.next_reference_start, aln.template_length]
+				out = [str(insertion), sample, aln.reference_name, aln.reference_start, aln.reference_end, aln.next_reference_name, aln.next_reference_start, aln.template_length]
 				out_dt.loc[len(dt)] = out
 	pdb.set_trace()
