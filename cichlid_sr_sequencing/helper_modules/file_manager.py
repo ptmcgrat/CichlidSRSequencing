301bd3d037c293ce124589903ef1c3d3feea1b50
Patrick McGrath <pmcgrath7@ipsec-10-2-97-139.vpn.gatech.edu>
2026-03-31 12:25:58 -0400

    Small updates

--- cichlid_sr_sequencing/helper_modules/file_manager.py
+++ cichlid_sr_sequencing/helper_modules/file_manager.py
@@ -166,7 +166,7 @@ class FileManager():
 		# Identify already run samples
 		filter_set = set(a_dt.SampleID)
 		already_run_samples = [x for x in set(self.sample_dt.SampleID) if x in filter_set]
-		samples = [x for x in set(self.s_dt.SampleID) if x not in filter_set]
+		samples = [x for x in set(self.samples_dt.SampleID) if x not in filter_set]
 		
 		if not rerun:
 			if len(already_run_samples) > 0:
