3151e3f4aa298a2e3632d699d4db410ff49ba212
Patrick McGrath <pmcgrath7@ipsec-10-2-97-139.vpn.gatech.edu>
2026-03-31 14:06:25 -0400

    Small updates

--- cichlid_sr_sequencing/downloadFiles.py
+++
@@ -1,4 +0,0 @@
-from helper_modules.file_manager_Replacement import FileManager as FM
-
-fm_obj = FM(genome_version = 'Mzebra_GT3')
-fm_obj.downloadData(fm_obj.localGenomesDir)
---
+++ cichlid_sr_sequencing/fixAlignmentDatabase.py
@@ -0,0 +1,21 @@
+from helper_modules.file_manager import FileManager as FM
+from helper_modules.alignment_worker import AlignmentWorker as AW
+import datetime
+
+for genome_version in ['Mzebra_GT3','Mconophoros_GT1']:
+	fm_obj = FM(genome_version = 'Mzebra_GT3')
+	aw_obj = AW(genome_version, fm_obj, check_size = False)
+	samples = fm_obj.returnCloudDirs(fm_obj.localBamRefDir)
+
+	for sample in samples:
+		print(samples + ', ' + str(datetime.datetime.now()))
+		fm_obj.createSampleFiles(sample)
+		fm_obj.downloadData(fm_obj.localSampleBamDir)
+		aw_obj.uploadAndUpdateDatabase(upload = False)
+
+		if sample not in fm_obj.sample_dt.SampleID.to_list():
+			print('Adding ' + sample + ' to SampleDatbase')
+			#sample_row = {'SampleID':sample,'Sex':'','Species':'','DoB':'','BroodID':'','Parents':'','Ecogroup':'','LabReared':''}
+			#fm_obj.sample_dt = fm_obj.sample_dt.append(pd.Series(sample_row), ignore_index = True)
+			#fm_obj._setDatabase('SampleDatabase', fm_obj.sample_dt)
+
