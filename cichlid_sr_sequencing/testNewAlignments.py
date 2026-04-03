2503f4b52e603c231ffa74fb3e545a64bfae8f5e
Patrick McGrath <pmcgrath7@ipsec-10-2-225-202.vpn.gatech.edu>
2026-04-03 14:50:06 -0400

    Small updates

--- cichlid_sr_sequencing/testNewAlignments.py
+++ cichlid_sr_sequencing/testNewAlignments.py
@@ -18,12 +18,13 @@ for uBam_file in fm_obj.localRawBamFiles:
 	command1 += ['--INTERLEAVE', 'true', '--NON_PF', 'true', '--TMP_DIR', fm_obj.localSampleTempDir]
 	output = subprocess.run(command1, capture_output = True)
 	timer.stop()
-	timer.start('Do it the old way')
+	"""
+	timer.start('Do it the new way')
 	command1 = ['gatk', 'SamToFastq', '-I', uBam_file, '--FASTQ', '/dev/stdout', '--CLIPPING_ATTRIBUTE', 'XT', '--CLIPPING_ACTION', '2']
 	command1 += ['--INTERLEAVE', 'true', '--NON_PF', 'true', '--TMP_DIR', fm_obj.localSampleTempDir]
-	command2 = ['bwa', 'mem', '-t', str(cpu_count()), '-M', '-p', fm_obj.localGenomeFile, '/dev/stdin']
+	command2 = ['minimap2', '-x','sr', '-a', '-t', str(cpu_count()), fm_obj.localGenomeFile, '-']
 	command3 = ['gatk', 'MergeBamAlignment', '-R', fm_obj.localGenomeFile, '--UNMAPPED_BAM', uBam_file, '--ALIGNED_BAM', '/dev/stdin']
-	command3 += ['-O', 'oldway.bam', '--ADD_MATE_CIGAR', 'true', '--CLIP_ADAPTERS', 'false', '--CLIP_OVERLAPPING_READS', 'true']
+	command3 += ['-O', 'newway.bam', '--ADD_MATE_CIGAR', 'true', '--CLIP_ADAPTERS', 'false', '--CLIP_OVERLAPPING_READS', 'true']
 	command3 += ['--INCLUDE_SECONDARY_ALIGNMENTS', 'true', '--MAX_INSERTIONS_OR_DELETIONS', '-1', '--PRIMARY_ALIGNMENT_STRATEGY', 'MostDistant']
 	command3 += ['--ATTRIBUTES_TO_RETAIN', 'XS', '--TMP_DIR', fm_obj.localSampleTempDir]
 
@@ -43,7 +44,7 @@ for uBam_file in fm_obj.localRawBamFiles:
 	command1 = ['bwa-mem2', 'mem', '-t', str(cpu_count()), '-M', '-p', fm_obj.localGenomeFile, uBam_file.replace('.bam','.fastq'), '-o','mem2_fq.sam']
 	output2 = subprocess.run(command1, capture_output = True)
 	timer.stop()
-	"""
+	
 	timer.start('Just minimap2')
 	command1 = ['minimap2', '-x','sr', '-a', '-t', str(cpu_count()), fm_obj.localGenomeFile, uBam_file.replace('.bam','.fastq'), '-o','mmap2_fq.sam']
 	output3 = subprocess.run(command1, capture_output = True)
