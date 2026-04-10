01b0734e05eb6ff11277e2a1215ee7fdb50856fa
Patrick McGrath <pmcgrath7@ipsec-10-2-225-202.vpn.gatech.edu>
2026-04-10 07:33:44 -0400

    Small updates

--- cichlid_sr_sequencing/alignFastQReads.py
+++ cichlid_sr_sequencing/alignFastQReads.py
@@ -32,7 +32,7 @@ fm_obj.downloadData(fm_obj.localGenomeDir)
 timer.stop()
 
 # Create alignment worker object:
-aw_obj = AW(args.Genome, fm_obj, check_size = True)
+aw_obj = AW(args.Genome, fm_obj, check_size = False)
 
 timer.start('  Parallel Downloading uBams files')
 #aw_obj.downloadReadData()
