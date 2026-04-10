b18a0be7b5e839fd5838614256fb84d74f769c19
Patrick McGrath <pmcgrath7@ipsec-10-2-225-202.vpn.gatech.edu>
2026-04-10 18:39:54 -0400

    Small updates

--- cichlid_sr_sequencing/addMetaInfoBlumer.py
+++ cichlid_sr_sequencing/addMetaInfoBlumer.py
@@ -13,11 +13,11 @@ fm_obj.readSampleDatabase()
 #fm_obj.downloadData(fm_obj.localReadDownloadDir)
 
 dt = pd.read_csv('../TempBioProjectData/Table_S1.tsv', sep = '\t')
-for idx, row in dt.iterrows():
+dt2 = pd.read_csv('../TempBioProjectData/Table_S7.tsv', sep = '\t')
+dt3 = pd.merge(dt,dt2,left_on='sequence_id', right_on='sample_id')
+for idx, row in dt3.iterrows():
 	if row.Biosample in fm_obj.sample_dt.SampleID.tolist():
-		if not pd.isna(row.sex):
-			fm_obj.sample_dt.loc[fm_obj.sample_dt.SampleID == row.Biosample,'Sex'] = row.sex
-		if not pd.isna(row.Inversion10):
-			fm_obj.sample_dt.loc[fm_obj.sample_dt.SampleID == row.Biosample,'Inversion10'] = row.Inversion10
+		if not pd.isna(row.chr10):
+			fm_obj.sample_dt.loc[fm_obj.sample_dt.SampleID == row.Biosample,'Inversion10'] = str(row.chr10)
 
 fm_obj.setSampleDatabase()
