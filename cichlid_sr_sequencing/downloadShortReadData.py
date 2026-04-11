bf9d15fa040d4b125be3edf4977be219edb395bc
Patrick McGrath <pmcgrath7@ipsec-10-2-225-202.vpn.gatech.edu>
2026-04-11 11:20:52 -0400

    Small updates

--- cichlid_sr_sequencing/downloadShortReadData.py
+++ cichlid_sr_sequencing/downloadShortReadData.py
@@ -129,6 +129,7 @@ for index, row in new_dt.iterrows():
 		for i, p in enumerate(processes):
 			if p.returncode == 0:
 				rows[i].FileSize = os.path.getsize(fm_obj.localReadsDir + rows[i].FileLocations)
+				pdb.set_trace()
 				fm_obj.addDNAReadRow(rows[i].drop(labels = ['Organism']))
 				try:
 					fm_obj.addSampleRow({'SampleID':rows[i].SampleID,'Sex':'','Species':rows[i].Organism,'DoB':'','BroodID':'','Parents':'','Ecogroup':'','LabReared':'','Inversion10':''})
