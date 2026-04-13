6c89f94995463a4da1b62e561c8eea84cca3ed8e
Patrick McGrath <pmcgrath7@ipsec-10-2-96-142.vpn.gatech.edu>
2026-04-13 10:16:03 -0400

    Small updates

--- cichlid_sr_sequencing/helper_modules/alignment_worker.py
+++ cichlid_sr_sequencing/helper_modules/alignment_worker.py
@@ -87,6 +87,7 @@ class AlignmentWorker():
 			data = SimpleNamespace(sampleID=sample, error_file = error_file, error_fp = open(error_file, 'w'), 
 					command = command, process = None)
 			current_processes.append(data)
+		pdb.set_trace()
 		for i in range(min(len(command_dict),num_parallel)):
 			data.process = subprocess.Popen(data.command, stderr = data.error_fp, stdout = subprocess.DEVNULL)
 			print('Starting ' + data.sampleID)
