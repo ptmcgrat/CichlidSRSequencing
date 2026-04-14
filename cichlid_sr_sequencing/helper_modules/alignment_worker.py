20f6801db4b21746b81d9c1ea1e95d9eb4a47675
Patrick McGrath <pmcgrath7@ipsec-10-2-225-202.vpn.gatech.edu>
2026-04-14 12:50:42 -0400

    Small updates

--- cichlid_sr_sequencing/helper_modules/alignment_worker.py
+++ cichlid_sr_sequencing/helper_modules/alignment_worker.py
@@ -75,7 +75,7 @@ class AlignmentWorker():
 				command_list.remove(data)  # Remove finished process from monitoring list
 				next_command = next((x for x in command_list if x.process is None),None)
 				if next_command is not None:
-					next_command.process = subprocess.Popen(next_command.command, stderr = next_comand.error_fp, stdout = subprocess.DEVNULL)
+					next_command.process = subprocess.Popen(next_command.command, stderr = next_command.error_fp, stdout = subprocess.DEVNULL)
 
 		return bad_samples
 
