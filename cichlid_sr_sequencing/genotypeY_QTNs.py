790831365b77cb0bde7a8e651f7ce50009540b7a
Patrick McGrath <pmcgrath7@ipsec-10-2-224-208.vpn.gatech.edu>
2026-05-12 11:38:44 -0400

    Small updates

--- cichlid_sr_sequencing/genotypeY_QTNs.py
+++ cichlid_sr_sequencing/genotypeY_QTNs.py
@@ -21,6 +21,8 @@ def print_sv(outfile, dt):
 fm_obj = FM(genome_version = 'Mzebra_GT3_NCBI')
 print(fm_obj.localNikeshDir + 'QTG_Candidates/')
 os.makedirs(fm_obj.localNikeshDir + 'QTG_Candidates/', exist_ok = True)
+os.makedirs(fm_obj.localErrorsDir, exist_ok = True)
+
 fm_obj.downloadData(fm_obj.localGenomeFile)
 
 fm_obj.readSampleDatabase()
@@ -30,7 +32,7 @@ aligned_samples = fm_obj.alignment_dt[fm_obj.alignment_dt.SampleID.isin(all_samp
 
 dt = pd.read_csv('candidateQTNs_all.tsv', sep = '\t')
 threshold = 10
-num_parallel = 1
+num_parallel = 48
 sv_vcf_file = fm_obj.localNikeshDir + 'QTG_Candidates/' + 'candidateQTNs_sv.vcf'
 sv_norm_vcf_file = fm_obj.localNikeshDir + 'QTG_Candidates/' + 'candidateQTNs_sv.norm.vcf.gz'
 lv_csv_file = fm_obj.localNikeshDir + 'QTG_Candidates/' + 'candidateQTNs_lv.csv'
@@ -40,24 +42,33 @@ dt[(dt.Alt.str.len() > threshold) | (dt.Reference.str.len() > threshold)].to_csv
 
 commands = []
 bad_samples = []
-for i,sampleID in enumerate(aligned_samples):
+for sampleID in aligned_samples:
     out_vcf = fm_obj.localNikeshDir + 'QTG_Candidates/' + sampleID + '_candidate_QTNs.vcf.gz'
     command = ['python','-m', 'unit_scripts.genotypeCandidates',sv_norm_vcf_file,lv_csv_file,out_vcf,'Mzebra_GT3_NCBI',sampleID]
+    error_file = fm_obj.localErrorsDir + 'QTGFinder_' + sampleID + '_errors.txt'
+    commands.append(SimpleNamespace(sampleID=sampleID, command = command, error_file = error_file))
+    
+for i,data in enumerate(commands):
+    
     if i < num_parallel:
-        print(' '.join(command))
-        commands.append(SimpleNamespace(sampleID=sampleID, command = command, process = subprocess.Popen(command, stderr = subprocess.DEVNULL, stdout = subprocess.DEVNULL)))
+        data.error_fp = open(data.error_file, 'w')
+        data.process = subprocess.Popen(data.command, stderr = data.error_fp, stdout = subprocess.DEVNULL)
     else:
-        commands.append(SimpleNamespace(sampleID=sampleID, command = command, process = None))
+        data.process = None
     
 while commands:
     finished_processes = [x for x in commands if x.process is not None and x.process.poll() is not None]
     for data in finished_processes:
+        data.error_fp.close()
         print(f'..{data.sampleID} complete..', end = '')
         if data.process.returncode != 0:
             bad_samples.append(data.sampleID)
+        else:
+            subprocess.run(['rm',data.error_file])
         command_list.remove(data)  # Remove finished process from monitoring list
-        next_command = next((x for x in command_list if x.process is None),None)
+        next_command = next((x for x in commands if x.process is None),None)
         if next_command is not None:
-            next_command.process = subprocess.Popen(next_command.command, stderr = subprocess.DEVNULL, stdout = subprocess.DEVNULL)
+            next_command.error_fp = open(data.error_file, 'w')
+            next_command.process = subprocess.Popen(next_command.command, stderr = data.error_fp, stdout = subprocess.DEVNULL)
 
 print(bad_samples)
