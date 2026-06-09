7d5193d7fdcbe9ae28a0f8b0486fbd77adbaba20
Patrick McGrath <pmcgrath7@ipsec-10-2-96-15.vpn.gatech.edu>
2026-06-09 17:11:03 -0400

    Small updates

--- cichlid_sr_sequencing/helper_modules/CandidateGenotyper.py
+++ cichlid_sr_sequencing/helper_modules/CandidateGenotyper.py
@@ -31,7 +31,7 @@ class CandidateGenotyper:
 
 	def _downloadData(self):
 		os.makedirs(self.fm_obj.localErrorsDir, exist_ok = True)
-		os.makedirs(self.localSampleVCFDir, exist_ok = True)
+		os.makedirs(self.masterSampleVCFDir, exist_ok = True)
 		self.fm_obj.downloadData(self.fm_obj.localGenomeFile)
 		self.fm_obj.downloadData(self.masterDir)
 
