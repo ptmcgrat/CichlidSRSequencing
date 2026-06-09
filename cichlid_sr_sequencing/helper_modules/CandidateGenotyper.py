94a2eaa0bf3291abd036e7c9800626d27f963829
Patrick McGrath <pmcgrath7@ipsec-10-2-96-15.vpn.gatech.edu>
2026-06-09 16:41:06 -0400

    Small updates

--- cichlid_sr_sequencing/helper_modules/CandidateGenotyper.py
+++ cichlid_sr_sequencing/helper_modules/CandidateGenotyper.py
@@ -63,8 +63,8 @@ class CandidateGenotyper:
 		commands = []
 		bad_samples = []
 		for sampleID in self.samples[0:2]:
-			out_vcf = self.masterSampleVCFDir + sampleID + '_candidate_QTNs.vcf.gz'
-			command = ['python','-m', 'unit_scripts.genotypeCandidates', self.masterSV_Norm_VCF, self.masterLV_Norm_VCF, self.genome_version, sampleID]
+			out_vcf = self.masterSampleVCFDir + sampleID + '_' + self.QTNs_ID + '.vcf.gz'
+			command = ['python','-m', 'unit_scripts.genotypeCandidates', self.masterSV_Norm_VCF, self.masterLV_Norm_VCF, out_vcf, self.genome_version, sampleID]
 		error_file = self.fm_obj.localErrorsDir + 'QTGFinder_' + sampleID + '_errors.txt'
 		commands.append(SimpleNamespace(sampleID=sampleID, command = command, error_file = error_file))
 	
