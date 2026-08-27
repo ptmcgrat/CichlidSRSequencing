d0d37ca11a0fb454a411e630dbac4921ca496f8e
Patrick McGrath <pmcgrath7@ipsec-10-2-98-137.vpn.gatech.edu>
2026-08-27 13:21:20 -0400

    Small updates

--- cichlid_sr_sequencing/addMetaInfoBlumer.py
+++ cichlid_sr_sequencing/addMetaInfoBlumer.py
@@ -15,13 +15,14 @@ dt1 = pd.read_csv(fm_obj.localReadDownloadDir + 'BlumerPaperData.csv')
 dt2 = pd.read_csv('../TempBioProjectData/Table_S1.tsv', sep = '\t')
 dt3 = pd.merge(dt1, dt2[['Biosample','clade']], left_on = 'BioSample', right_on='Biosample', how = 'left')
 dt3.to_csv(fm_obj.localReadDownloadDir + 'BlumerPaperData_withClades.csv')
-pdb.set_trace()
 # Download and open run info file that contains new data to include
 #fm_obj.downloadData(fm_obj.localReadDownloadDir)
 
 dt = pd.read_csv('../TempBioProjectData/Table_S1.tsv', sep = '\t')
 dt2 = pd.read_csv('../TempBioProjectData/Table_S7.tsv', sep = '\t')
 dt3 = pd.merge(dt,dt2,left_on='sequence_id', right_on='sample_id')
+pdb.set_trace()
+
 for idx, row in dt3.iterrows():
 	if row.Biosample in fm_obj.sample_dt.SampleID.tolist():
 		if not pd.isna(row.chr10):
